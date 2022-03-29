fa788ad597772289dc7f8f709bda5e0d0d8648f6
kim-jos <77760709+kim-jos@users.noreply.github.com>
2022-03-29 12:11:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# add homebrew path
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # install programs in brewFile
 brew bundle --file=./Brewfile
